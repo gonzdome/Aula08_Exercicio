08e9899b65382274d15553957d7bbabb1f941966
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-08-16 20:24:41 -0300

    Add project files.

---
+++ Aula08_Exercicio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula08_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //transporte, xérox, cantina, apm
+
+            Console.WriteLine("Quanto foi gasto em transporte?");
+            double n1 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Quanto foi gasto em xérox?");
+            double n2 = double.Parse(Console.ReadLine());
+
+           Console.WriteLine("Quanto foi gasto em cantina?");
+            double n3 = double.Parse(Console.ReadLine());
+
+           Console.WriteLine("Quanto foi gasto em apm?");
+            double n4 = double.Parse(Console.ReadLine());
+
+
+            double r = n1 + n2 + n3 + n4;
+
+            Console.WriteLine("O total gasto foi de R$" + r + "!");
+
+            Console.ReadKey();
+        }
+    }
+}
